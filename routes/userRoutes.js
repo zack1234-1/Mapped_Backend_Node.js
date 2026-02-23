81ad1679bcb9e6563e0862eabe8b58c8ed50ddac
Wz <jaxksonlau361@gmail.com>
2026-02-24 01:00:04 +0800

    fix the email error

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,12 +6,12 @@ const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
+  host: 'smtp-relay.brevo.com',
   port: 587,
   secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS,
   },
 });
 

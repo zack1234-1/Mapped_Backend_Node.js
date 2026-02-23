93fe4e6eb0d94b62c8472690ebe70472a0cf3236
Wz <jaxksonlau361@gmail.com>
2026-02-24 00:44:55 +0800

    fix the email error

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,13 +5,19 @@ const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
 
-// Create transporter for email
 const transporter = nodemailer.createTransport({
-  service: 'Gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+});
+
+transporter.verify((error, success) => {
+  if (error) console.log('Local test failed:', error);
+  else console.log('Local test succeeded – ready to send');
 });
 
 // Email helper function
